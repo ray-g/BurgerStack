87205d6b9f5ab1add8d0e7da779d16f05a81384c
Ray Guo <ray_g@live.com>
2016-12-17 01:41:23 +0800

    change to burgerstack

--- docker-env/postgres/initdb.d/init_user_db.sh
+++ docker-env/postgres/initdb.d/init_user_db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER warmpet WITH PASSWORD 'changeme';
-    CREATE DATABASE warmpet_dev;
-    GRANT ALL PRIVILEGES ON DATABASE warmpet_dev TO warmpet;
+    CREATE USER burgerstack WITH PASSWORD 'changeme';
+    CREATE DATABASE burgerstack_dev;
+    GRANT ALL PRIVILEGES ON DATABASE burgerstack_dev TO burgerstack;
 EOSQL
