8cd300ccb59587856645834afe8308f36894c74e
Ray Guo <ray_g@live.com>
2016-12-20 00:00:21 +0800

    browsersync singleton for reload

---
+++ tools/gulp/libs/browsersync.ts
@@ -0,0 +1,58 @@
+import { Config } from '../../../config/config';
+import * as browserSync from 'browser-sync';
+
+const config = <any>Config.getInstance().getConfig();
+
+export class BrowserSync {
+  private static _instance: BrowserSync = new BrowserSync();
+
+  private browserSync: any; // browserSync.BrowserSyncInstance;
+
+  public static getInstance(): BrowserSync {
+    return BrowserSync._instance;
+  }
+
+  constructor() {
+    if (BrowserSync._instance) {
+      throw new Error('Error: Instantiation failed: Use BrowserSync.getInstance() instead of new.');
+    }
+    this.initBrowserSync();
+    BrowserSync._instance = this;
+  }
+
+  private initBrowserSync() {
+    if (config.browserSync && config.browserSync.enable === true) {
+      this.browserSync = browserSync.create();
+    }
+  }
+
+  public startServer() {
+    if (this.browserSync) {
+      let server = (process.env.NODE_ENV === 'secure' ? 'https://' : 'http://') + config.host + ':' + config.port;
+
+      this.browserSync.init(null, {
+        proxy: server,
+        logFileChanges: false,
+        files: [],
+        browser: 'default',
+        port: config.browserSync.port,
+        open: false
+      });
+    }
+  }
+
+  public delayReload() {
+    if (this.browserSync) {
+      setTimeout(function () {
+        this.browserSync.reload({ stream: false });
+      }, 3000); // delay 3 seconds due to node restart.
+    }
+  }
+
+  public reload() {
+    if (this.browserSync) {
+      console.log('File changed, refreshing browser...');
+      this.browserSync.reload();
+    }
+  }
+}
--- tools/gulp/tasks/browsersync.ts
+++ tools/gulp/tasks/browsersync.ts
@@ -1,19 +1,5 @@
-import { Config } from '../../../config/config';
-
-let config = <any>Config.getInstance().getConfig();
+import { BrowserSync } from '../libs/browsersync';
 
 export = () => {
-  if (config.browserSync && config.browserSync.enable === true) {
-    let server = (process.env.NODE_ENV === 'secure' ? 'https://' : 'http://') + config.host + ':' + config.port;
-
-    let browserSync = require('browser-sync').create();
-    browserSync.init(null, {
-      proxy: server,
-      logFileChanges: false,
-      files: [],
-      browser: 'default',
-      port: config.browserSync.port,
-      open: false
-    });
-  }
+  BrowserSync.getInstance().startServer();
 };
