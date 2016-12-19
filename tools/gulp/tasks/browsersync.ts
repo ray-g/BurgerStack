8de9b48248ced9f6d58c18ca2e1e280236fa2f49
Ray Guo <ray_g@live.com>
2016-12-19 22:48:38 +0800

    remove debug outputs.

--- tools/gulp/tasks/browsersync.ts
+++ tools/gulp/tasks/browsersync.ts
@@ -3,12 +3,10 @@ import { Config } from '../../../config/config';
 let config = <any>Config.getInstance().getConfig();
 
 export = () => {
-  console.log(config);
   if (config.browserSync && config.browserSync.enable === true) {
     let server = (process.env.NODE_ENV === 'secure' ? 'https://' : 'http://') + config.host + ':' + config.port;
 
     let browserSync = require('browser-sync').create();
-    console.log(browserSync);
     browserSync.init(null, {
       proxy: server,
       logFileChanges: false,
