766bc33a494fcd8be36e343d2dd49a508670118c
Ray Guo <ray_g@live.com>
2016-12-19 23:33:27 +0800

    method access

--- config/config.ts
+++ config/config.ts
@@ -140,7 +140,7 @@ export class Config {
   /**
   * Initialize global configuration
   */
-  public initGlobalConfig() {
+  private initGlobalConfig() {
     // Validate NODE_ENV existence
     this.validateEnvironmentVariable();
 
