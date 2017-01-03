7610bd33cdf70c37cb0c7972bd446e3cddf37086
Ray Guo <ray_g@live.com>
2017-01-03 17:17:58 +0800

    add more ut

--- test/server/config/utils.common.ts
+++ test/server/config/utils.common.ts
@@ -1,6 +1,8 @@
 import * as _ from 'lodash';
 import chai = require('chai');
-import { assetsUnion } from '../../../config/utils/common';
+import sinon = require('sinon');
+import * as utilsCommon from '../../../config/utils/common';
+import * as glob from 'glob';
 
 let expect = chai.expect;
 
@@ -18,13 +20,13 @@ function isStringArray(value: any) {
   return false;
 }
 
-describe('config.utils.common:', () => {
+describe('config/utils/common:', () => {
 
-  describe('assetsUnion', () => {
-    it('return type should be string[]', (done) => {
+  describe('assetsUnion()', () => {
+    it('should return a string[]', (done) => {
       let arr1 = ['aaa', 'bbb'];
       let arr2 = ['ccc', 'ddd'];
-      let assets = assetsUnion(arr1, arr2);
+      let assets = utilsCommon.assetsUnion(arr1, arr2);
       expect(isStringArray(assets)).to.equals(true);
       done();
     });
@@ -32,7 +34,7 @@ describe('config.utils.common:', () => {
     it('should return correct array', (done) => {
       let arr1 = ['aaa', 'bbb'];
       let arr2 = ['ccc', 'ddd'];
-      let assets = assetsUnion(arr1, arr2);
+      let assets = utilsCommon.assetsUnion(arr1, arr2);
       expect(_.isEqual(assets, ['aaa', 'bbb', 'ccc', 'ddd'])).to.equals(true);
       done();
     });
@@ -40,8 +42,72 @@ describe('config.utils.common:', () => {
     it('should return combined array', (done) => {
       let arr1 = ['aaa', 'bbb'];
       let arr2 = ['bbb', 'ddd'];
-      let assets = assetsUnion(arr1, arr2);
+      let assets = utilsCommon.assetsUnion(arr1, arr2);
       expect(_.isEqual(assets, ['aaa', 'bbb', 'ddd'])).to.equals(true);
+      done();
+    });
+  });
+
+  describe('getGlobbedPaths()', () => {
+    beforeEach(() => {
+      sinon.stub(glob, 'sync')
+      .onFirstCall().returns(['aaa', 'bbb', 'ccc', 'exclude1'])
+      .onSecondCall().returns(['xxx', 'yyy', 'zzz', 'exclude2']);
+    });
+
+    afterEach(() => {
+      (<any>glob.sync).restore();
+    });
+
+    it('should return a string[]', (done) => {
+      let paths = utilsCommon.getGlobbedPaths('a', '');
+      expect(isStringArray(paths)).to.equals(true);
+      done();
+    });
+
+    it('should return 4 items', (done) => {
+      let paths = utilsCommon.getGlobbedPaths('a', '');
+      expect(paths.length).to.equal(4);
+      done();
+    });
+
+    it('should return 4 items withiout "exclude1" when exclude provide', (done) => {
+      let paths = utilsCommon.getGlobbedPaths('a', 'exclude1');
+      let hasExclude = false;
+      paths.forEach((path) => {
+        if (path.includes('exclude')) {
+          hasExclude = true;
+        }
+      });
+      expect(paths.length).to.equal(4);
+      expect(hasExclude).to.equal(false);
+      done();
+    });
+
+    it('should return 7 items when exclude provide and 2 patterns provide', (done) => {
+      let paths = utilsCommon.getGlobbedPaths(['a', 'b'], ['exclude1', 'exclude2']);
+      let hasExclude = false;
+      paths.forEach((path) => {
+        if (path.includes('exclude')) {
+          hasExclude = true;
+        }
+      });
+      expect(paths.length).to.equal(7);
+      expect(hasExclude).to.equal(false);
+      done();
+    });
+
+    it('should return url itself', (done) => {
+      let fakeUrl = 'http://fake/url';
+      let paths = utilsCommon.getGlobbedPaths(['a', 'b', fakeUrl], ['exclude1', 'exclude2']);
+      let hasUrl = false;
+      paths.forEach((path) => {
+        if (path === fakeUrl) {
+          hasUrl = true;
+        }
+      });
+      expect(paths.length).to.equal(8);
+      expect(hasUrl).to.equal(true);
       done();
     });
   });
