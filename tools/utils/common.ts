63aebfa11e578c8226f04b6bc6445a863f40e2c4
Ray Guo <ray_g@live.com>
2017-01-03 22:42:01 -0800

    refactor code

--- tools/utils/common.ts
+++ tools/utils/common.ts
@@ -1,16 +1,12 @@
 export function isStringArray(value: any) {
   let isStrArr = true;
-  if (value instanceof Array) {
-    if (value.length < 1) {
-      isStrArr = false;
-    } else {
-      value.forEach((item) => {
-        if (typeof item !== 'string') {
-          isStrArr = false;
-          return;
-        }
-      });
-    }
+  if (value instanceof Array && value.length > 0) {
+    value.forEach((item) => {
+      if (typeof item !== 'string') {
+        isStrArr = false;
+        return;
+      }
+    });
   } else {
     isStrArr = false;
   }
