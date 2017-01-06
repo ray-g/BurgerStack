f486a41f92a8d9a7bc37f37a43d02114ac3589fc
Ray Guo <ray_g@live.com>
2017-01-06 17:56:54 +0800

    coveralls not running

--- tools/gulp/tasks/coveralls.ts
+++ tools/gulp/tasks/coveralls.ts
@@ -4,6 +4,6 @@ const plugins = <any>gulpLoadPlugins();
 const config = require('../config');
 
 export = (done: any) => {
-  gulp.src(config.tests.coverageDir + '/**/lcov.info')
-    .pipe(plugins.if(require('is-ci'), plugins.coveralls()));
+  return gulp.src(config.tests.coverageDir + '/**/lcov.info')
+    .pipe(plugins.coveralls());
 };
