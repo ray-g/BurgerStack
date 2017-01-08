fa171248156db51be4cf4516bfda3b09c44dd78f
Ray Guo <ray_g@live.com>
2017-01-09 00:03:20 +0800

    add UT to cover entry

---
+++ test/server/server.entry.spec.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+import { resolve } from 'path';
+import sinon = require('sinon');
+
+import { AppServer } from '../../config/libs/server';
+
+describe('Server entry file', () => {
+  it('should call server start', () => {
+    let startStub = sinon.stub(AppServer, 'start');
+    let chdirStub = sinon.stub(process, 'chdir', () => { });
+    require(resolve('dist/server.js'));
+    expect(chdirStub.calledWith('dist'));
+    expect(startStub.called);
+    chdirStub.restore();
+    startStub.restore();
+  });
+});
