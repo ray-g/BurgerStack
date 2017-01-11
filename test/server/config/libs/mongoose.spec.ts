48bbd1b6da1393d3c1aaf5ef591d0c18302923e3
Ray Guo <ray_g@live.com>
2017-01-12 00:02:33 +0800

    add ut for mongoose

---
+++ test/server/config/libs/mongoose.spec.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+// let sinon = require('sinon');
+// require('sinon-as-promised');
+
+import { Mongoose } from '../../../../config/libs/mongoose';
+
+describe('Mongoose class', () => {
+  let stubs: any[] = [];
+
+  afterEach(() => {
+     stubs.forEach((stub) => {
+       stub.restore();
+     });
+    stubs = [];
+  });
+
+   describe('Singleton staffs', () => {
+    it('.getInstance(), should return an Mongoose instance.', () => {
+      expect(Mongoose.getInstance()).instanceOf(Mongoose);
+    });
+
+    it('new Mongoose instance should get an error', () => {
+      expect(() => { new Mongoose(); }).throws('Error: Instantiation failed: Use Mongoose.getInstance() instead of new.');
+    });
+  });
+});
