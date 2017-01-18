a3ef0bccb43481d4506b10936623582d636d450e
Ray Guo <ray_g@live.com>
2017-01-19 01:36:51 +0800

    change config

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*.ts'
+      'test/client/**/*.ts'
     ],
 
 
@@ -53,7 +53,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    // browsers: ['PhantomJS'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
