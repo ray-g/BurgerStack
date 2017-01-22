6067812d2e29468fde820ccb5d4186121b718303
Ray Guo <ray_g@live.com>
2017-01-23 00:55:19 +0800

    add karma report task

---
+++ tools/gulp/tasks/karma.report.ts
@@ -0,0 +1,25 @@
+import * as gulp from 'gulp';
+import * as gulpLoadPlugins from 'gulp-load-plugins';
+import { instrument } from '../libs/instrument';
+import Config from '../config';
+
+const isparta = require('isparta');
+const remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
+const plugins = <any>gulpLoadPlugins();
+
+export = (done: any) => {
+  let coverageDir = Config.tests.coverageDir + '/karma';
+
+  instrument(Config.dist.allJS, coverageDir, gulp, plugins, isparta, true)
+    .on('finish', () => {
+      gulp.src(coverageDir + '/coverage-final.json')
+        .pipe(remapIstanbul({
+          basePath: '.',
+          reports: {
+            'html': coverageDir + '/html',
+            'text-summary': null,
+            'lcovonly': coverageDir + '/lcov.info'
+          }
+        }));
+    });
+};
--- tools/gulp/tasks/karma.run.ts
+++ tools/gulp/tasks/karma.run.ts
@@ -1,26 +1,11 @@
 import { startKarma } from '../../utils/karma.start';
-
-import * as gulp from 'gulp';
-import * as gulpLoadPlugins from 'gulp-load-plugins';
-import { assetsUnion } from '../../../config/utils';
-import { instrument } from '../libs/instrument';
 import Config from '../config';
 
-const isparta = require('isparta');
-const remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
-const plugins = <any>gulpLoadPlugins();
 /**
  * Executes the build process, running all unit tests using `karma`.
  */
 export = (done: any) => {
-  // let testSuites = assetsUnion(Config.tests.js.client);
-  // let error: any = null;
   let coverageDir = Config.tests.coverageDir + '/karma';
-
-  instrument(Config.dist.allJS, coverageDir, gulp, plugins, isparta, true)
-    .on('finish', () => {
-
-    });
 
   return startKarma(done, {
     preprocessors: {
