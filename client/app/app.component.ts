4af22f513668fa112a0ad9cec05a6a2ba524af8f
Ray Guo <ray_g@live.com>
2017-01-22 23:58:34 +0800

    modify ng2 test sample

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -2,6 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`
+  template: `
+    <h1>Hello {{name}}</h1>
+    <main>
+      <h2>Value: {{ value }}</h2>
+      <button class="increment" (click)="onIncrementClick()">+</button>
+      <button class="decrement" (click)="onDecrementClick()">-</button>
+    </main>
+  `
 })
-export class AppComponent { name = 'BurgerStack'; }
+export class AppComponent {
+  name = 'BurgerStack';
+  value = 0;
+
+  onIncrementClick() {
+    this.value = Math.min(100, ++this.value);
+  }
+
+  onDecrementClick() {
+    this.value = Math.max(-100, --this.value);
+  }
+}
--- test/client/app/app.component.spec.ts
+++ test/client/app/app.component.spec.ts
@@ -1,11 +1,53 @@
-import { expect } from 'chai';
-
-require('core-js');
+import { getTestBed, TestBed } from '@angular/core/testing';
 import { AppComponent } from '../../../client/app/app.component';
+import { By } from '@angular/platform-browser';
+import { expect } from 'chai';
+import { spy } from 'sinon';
 
 describe('AppComponent', () => {
-  it('name should be "BurgerStack"', () => {
-    let appComp = new AppComponent();
-    expect(appComp.name).to.equal('BurgerStack');
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    });
   });
+
+  afterEach(() => {
+    getTestBed().resetTestingModule();
+  });
+
+  it('should display 0 as initial value', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+
+    fixture.detectChanges();
+
+    const h2 = fixture.debugElement.query(By.css('h2'));
+
+    expect(h2.nativeElement.textContent).to.equal('Value: 0');
+  });
+
+  it('should increment the value', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+
+    fixture.componentInstance.onIncrementClick();
+
+    fixture.detectChanges();
+
+    const h2 = fixture.debugElement.query(By.css('h2'));
+
+    expect(h2.nativeElement.textContent).to.equal('Value: 1');
+  });
+
+  it('should invoke onIncrementClick when the user clicks the increment button', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+
+    const onIncrementClick = spy(fixture.componentInstance, 'onIncrementClick');
+
+    const button = fixture.debugElement.query(By.css('.increment'));
+
+    button.triggerEventHandler('click', {});
+
+    expect(onIncrementClick.called).to.equal(true);
+  });
+
+  /* snip */
 });
