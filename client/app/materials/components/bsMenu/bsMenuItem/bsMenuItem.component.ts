2e40a5fedc07f936bd0e5c68ef88dc56acf5578a
Ray Guo <ray_g@live.com>
2017-02-02 20:24:40 +0800

    add menu item component for sidebar

---
+++ client/app/materials/components/bsMenu/bsMenuItem/bsMenuItem.component.ts
@@ -0,0 +1,26 @@
+import { Component, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'bs-menu-item',
+  encapsulation: ViewEncapsulation.None,
+  styles: [require('./bsMenuItem.scss')],
+  template: require('./bsMenuItem.html')
+})
+export class BsMenuItem {
+
+  @Input() menuItem: any;
+  @Input() child: boolean = false;
+
+  @Output() itemHover = new EventEmitter<any>();
+  @Output() toggleSubMenu = new EventEmitter<any>();
+
+  public onHoverItem($event: any): void {
+    this.itemHover.emit($event);
+  }
+
+  public onToggleSubMenu($event: any, item: any): boolean {
+    $event.item = item;
+    this.toggleSubMenu.emit($event);
+    return false;
+  }
+}
